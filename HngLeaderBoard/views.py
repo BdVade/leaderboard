c557e623a0b1bb33837a8485c94095cddfa5ecb4
BdVade <victoraderibigbe03@gmail.com>
2020-06-23 00:14:58 +0200

    initial structure

---
+++ HngLeaderBoard/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import View
+import json
+import os
+
+with open('../leaderboard/HngLeaderBoard/package.json') as f:
+    data = json.load(f)
+
+
+class LeaderboardView(View):
+
+    def get(self, request):
+        params = {"data": data}
+        return render(request, 'index.html', params)
+
